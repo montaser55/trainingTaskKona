658534aec9732ce35c10028d45be78a84a0d4b63
montaser55 <majidmontaser@gmail.com>
2021-03-15 14:34:32 +0600

    consuming REST api

---
+++ consumeRESTapi/src/main/java/com/example/demo/ConsumeResTapiApplication.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+
+@SpringBootApplication
+public class ConsumeResTapiApplication {
+	
+	private static final Logger log=LoggerFactory.getLogger(ConsumeResTapiApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumeResTapiApplication.class, args);
+	}
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+	
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) {
+		return args->{
+			Quote quote=restTemplate.getForObject("https://quoters.apps.pcfone.io/api/random", Quote.class);
+			log.info(quote.toString());
+		};
+	}
+
+}
